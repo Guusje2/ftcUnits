1c17394eab01137e05b62a5adbb947b0bdd77036
Ekiem13 <45897842+Ekiem13@users.noreply.github.com>
2019-02-02 14:28:00 +0100

    armservo
    heb een arm servo toegevoed voor omhoog en naar beneden

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOp2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOp2.java
@@ -21,14 +21,14 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 
 public class DriverOp2 extends OpMode {
 
-
+    private float ArmServo1Pos = 0;
     private DcMotor MotorFrontRight;
     private DcMotor MotorFrontLeft;
     private DcMotor MotorBackRight;
     private DcMotor MotorBackLeft;
+    private DcMotor ArmMotor;
     private Servo BlockBoxServo;
-    private CRServo ElementServo1;
-    private CRServo ELementServo2;
+    private Servo ArmServo1;
     public  boolean IsControlled = true;
     private DcMotor LiftMotor;
     double driveDirectionSpeed = 1 ;
@@ -45,20 +45,11 @@ public class DriverOp2 extends OpMode {
         MotorBackRight  = hardwareMap.dcMotor.get("MotorBackRight");
         MotorFrontLeft  = hardwareMap.dcMotor.get("MotorFrontLeft");
         MotorFrontRight = hardwareMap.dcMotor.get("MotorFrontRight");
+        ArmMotor = hardwareMap.dcMotor.get("ArmMotor");
         LiftMotor = hardwareMap.dcMotor.get("LiftMotor");
         BlockBoxServo = hardwareMap.servo.get("BlockBoxServo");
-        ElementServo1 = hardwareMap.crservo.get("ElementServo1");
-        ELementServo2 = hardwareMap.crservo.get("ElementServo2");
-
-      //  ArmMotor = hardwareMap.dcMotor.get("ArmMotor");
-       // BekServo1 = hardwareMap.servo.get("BekServo1");
-       /// BekServo2 = hardwareMap.servo.get("BekServo2");
-       // BakMotor = hardwareMap.dcMotor.get("BakMotor");
-      //  testSensor = hardwareMap.colorSensor.get("testSensor");
+        ArmServo1 = hardwareMap.servo.get("ArmServo1");
         driveDirectionSpeed  = 1;
-     //   BekServo2.setDirection(Servo.Direction.REVERSE);
-     //   BekServo1.setPosition(1);
-     //   BekServo2.setPosition(1);
         try {
             logUtils.StartLogging(1);
         } catch (Exception e) {
@@ -73,9 +64,6 @@ public class DriverOp2 extends OpMode {
 
         SpeedChecks();
         DriveChecks();
-
-      //  ArmChecks();
-        //telemetry.addData("color",testSensor.alpha());
 
 
     }
@@ -133,6 +121,8 @@ public class DriverOp2 extends OpMode {
         } else {
             LiftMotor.setPower(-gamepad2.left_stick_y);
         }
+
+        ArmMotor.setPower(-gamepad2.right_stick_y);
         if(gamepad2.a){
                 isLocked = true;
             }
@@ -140,16 +130,9 @@ public class DriverOp2 extends OpMode {
             isLocked = false;
         }
 
-        //CRServoChecks element box
-        if(gamepad2.dpad_down){
-            MineralSystemOn();
-        }
-        if(gamepad2.dpad_up){
-            MineralSystemInverted();
-        } if (gamepad2.dpad_right || gamepad2.dpad_left){
-            MineralSystemOff();
-        }
-
+        ArmServo1.setPosition(ArmServo1Pos);
+        ArmServo1Pos += 0.1* gamepad2.right_trigger;
+        ArmServo1Pos -= 0.1* gamepad2.left_trigger;
     }
 
         public void sidemoving(int speed) {
@@ -174,19 +157,7 @@ public class DriverOp2 extends OpMode {
         BlockBoxServo.setPosition(.3);
     }
 
-    public void MineralSystemOn() {
-        ElementServo1.setPower(1);
-        ELementServo2.setPower(-1);
-    }
 
-    public void MineralSystemInverted(){
-        ElementServo1.setPower(-1);
-        ELementServo2.setPower(1);
-    }
-    public void MineralSystemOff(){
-        ElementServo1.setPower(0);
-        ELementServo2.setPower(0);
-    }
 }
 
 

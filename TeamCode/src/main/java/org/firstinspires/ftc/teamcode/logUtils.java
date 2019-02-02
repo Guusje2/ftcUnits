c05bcd3787929572a0f22d8947af986e8378e08a
Guus Dohmen <guusdohmen@hotmail.nl>
2019-02-02 17:17:57 +0100

    more comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/logUtils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/logUtils.java
@@ -1,47 +1,48 @@
 package org.firstinspires.ftc.teamcode;
 
-import android.app.Application;
-import android.os.Environment;
-import android.widget.Toast;
 
+import android.os.Environment;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Time;
 import java.util.Calendar;
 import java.util.HashMap;
 
 /**
  * Created by guusd on 7/20/2018.
- * for logging in the ftc to a text file
+ * for logging in the ftc to a .csv file
  */
 
 public class logUtils {
 
-    public static enum logType {
+    /** the type of logmessage, used for sorting in a spreadsheet editor*/
+    public  enum logType {
         normal,
         warning,
         error
     };
+    /**name of the FTC team, used in the file namesw */
     static public String teamname = "FTCUnits";
     FileOutputStream fOUT = null;
+    /**the list of filewriters, can be seen as the list of logging channels */
     static public HashMap<Integer, FileWriter> filewriters;
-    static public boolean isOpModeRunning = false;
 
     public static void StartLogging(Integer id) throws IOException {
         String FileName =  teamname + Calendar.getInstance().getTime().toString() + ".csv";
         File file = new File(getPublicAlbumStorageDir(teamname),FileName );
         if(filewriters == null){
-        filewriters = new HashMap<Integer, FileWriter>();
+            filewriters = new HashMap<Integer, FileWriter>();
         }
-        filewriters. put(id,new FileWriter(file));
+        filewriters.put(id,new FileWriter(file));
     }
 
+    /**sets the FTC team name, used in the file name */
     public static void setTeamname(String _teamname){
         teamname = _teamname;
     }
+
+    /**gets the public image folder */
     public  static File getPublicAlbumStorageDir(String albumName) {
         // Get the directory for the user's public pictures directory.
         File file = new File(Environment.getExternalStoragePublicDirectory(
@@ -52,6 +53,8 @@ public class logUtils {
         return file;
     }
 
+
+    /**closes the filewriter given by the id, do this to make sure your data gets saved */
     public static void StopLogging(int id) {
         try {
             filewriters.get(id).close();
@@ -60,6 +63,7 @@ public class logUtils {
         }
     }
 
+    /** log your data to the filewriter given by the ID */
     public static void Log(logType type, String message, int id) {
 
         try {

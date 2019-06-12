8ef42d0e0cdcef9f327f27d17f04e843e2526927
Guus Dohmen <guusdohmen@hotmail.nl>
2019-06-12 09:46:07 +0200

    Vector functions added
    added subtract and add functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MathEssentials/Vector2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MathEssentials/Vector2.java
@@ -7,6 +7,21 @@ public class Vector2 {
     public double DistanceToVector2(Vector2 b) {
         return Math.sqrt((this.X - b.X)*(this.X - b.X) + (this.Y - b.Y)*(this.Y - b.Y));
     }
+    
+    public Vector2 subtract (Vector2 b) {
+     Vector2 a = new Vector2();
+        a.X = this.X - b.X;
+        a.Y = this.Y - b.Y;
+        return a;
+    }
+    
+    public Vector2 add (Vector2 b) {
+     Vector2 a = new Vector2();
+        a.X = this.X + b.X;
+        a.Y = this.Y +b.Y;
+        return a;
+    }
+    
 
 
 }
